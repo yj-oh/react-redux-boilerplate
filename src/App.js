d2b12972a1faf005196c733180af1e47c60197db
yj-oh <holidayoh99@gmail.com>
2020-11-19 02:43:31 +0900

    [수정] App.js | 기존 코드 삭제 및 Route example code 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
+//import { Route, Switch } from 'react-router-dom';
 
-function App() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+//import Index from './index';
+
+const App = () => {
+	// return (
+	// 	<Switch>
+	// 		<Route component={Index} path='/' exact />
+	// 	</Switch>
+	// );
+};
 
 export default App;
